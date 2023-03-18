e39f6e2fb61da94727abd0d00f3292e373cb2b8a
djamn <david@jamnig.net>
2023-03-18 11:45:59 +0100

    updated package name

---
+++ src/main/java/net/jamnigdippold/Main.java
@@ -0,0 +1,13 @@
+package net.jamnigdippold;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+    }
+}
--- src/test/java/net/jamnigdippold/MainTest.java
+++ src/test/java/net/jamnigdippold/MainTest.java
@@ -1,4 +1,4 @@
-package net.jamnig;
+package net.jamnigdippold;
 
 public class MainTest {
 
