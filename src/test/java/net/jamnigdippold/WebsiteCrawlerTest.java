f1b77b5bd41c3ae828b496bc4092ec5aaca28bc1
Benjamin Dippold <dippold.benjamin@gmail.com>
2023-04-04 13:20:53 +0200

    Updated Mocking to remove Jsoup/Internet dependency

--- src/test/java/net/jamnigdippold/WebsiteCrawlerTest.java
+++ src/test/java/net/jamnigdippold/WebsiteCrawlerTest.java
@@ -1,48 +1,44 @@
 package net.jamnigdippold;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class WebsiteCrawlerTest {
-    @Mock
-    private static Connection mockedConnection;
-    @Mock
+
     private static Document mockedDocument;
     private static WebsiteCrawler webCrawler;
     static String htmlMock = "<html><body><h1>Heading h1</h1></body></html>";
 
     @BeforeEach
-    void setUp() throws IOException {
-        MockitoAnnotations.openMocks(this);
-        mockedDocument = Jsoup.parse(htmlMock);
-        when(mockedConnection.get()).thenReturn(mockedDocument);
-        when(Jsoup.connect(anyString())).thenReturn(mockedConnection);
-
-        webCrawler = new WebsiteCrawler("https://example.com", 1, "de"); // Todo muss noch auf die jeweiligen Sachen angepasst werden
+    public void setUp(){
+        setUpJsoupMock();
         webCrawler.establishConnection();
-//        webCrawler.setWebsiteDocumentConnection(Jsoup.parse(htmlMock));
     }
 
+    private void setUpJsoupMock() {
+        MockitoAnnotations.openMocks(this);
+        mockedDocument = Jsoup.parse(htmlMock);
+        webCrawler = spy(new WebsiteCrawler("https://example.com", 1, "de"));// Todo muss noch auf die jeweiligen Sachen angepasst werden
+
+        doAnswer(invocationOnMock -> {
+            webCrawler.setWebsiteDocumentConnection(mockedDocument);
+            return null;
+        }).when(webCrawler).establishConnection();
+    }
 
     @AfterAll
-    static void tearDown() {
+    public static void tearDown() {
         webCrawler = null;
     }
 
